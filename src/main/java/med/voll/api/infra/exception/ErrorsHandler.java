7c7b36cad1d5d8c7410096c3e92586eaa30ec5c0
Fuzus <matheusdamata2000@gmail.com>
2023-07-21 15:41:31 -0300

    Adicionando tratamento para erros comuns

--- src/main/java/med/voll/api/infra/exception/ErrorsHandler.java
+++ src/main/java/med/voll/api/infra/exception/ErrorsHandler.java
@@ -1,7 +1,13 @@
 package med.voll.api.infra.exception;
 
 import jakarta.persistence.EntityNotFoundException;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,6 +25,31 @@ public class ErrorsHandler {
     public ResponseEntity handleError400(MethodArgumentNotValidException ex){
         var errors = ex.getFieldErrors();
         return ResponseEntity.badRequest().body(errors.stream().map(ValidationErrorsData::new).toList());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity handleError400(HttpMessageNotReadableException ex){
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity handleErrorBadCredentials(){
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credentiais inválidas");
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity handleAuthenticationError(){
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Falha na autenticação");
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity handleAccessDeniedError(){
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Acesso Negado!");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity handleInternalServerError(Exception ex){
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro: " + ex.getLocalizedMessage());
     }
 
 
